f7a5dcc4168b9bf99961651b9e82eed572fd76e7
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-05-16 13:01:29 -0300

    feat: description of the calculation

--- cosine-similarity.py
+++ cosine-similarity.py
@@ -1,2 +1,4 @@
 def cosine_similarity(vector1: list[float], vector2: list[float]) -> float:
     """Returns the cosine of the angle between two vectors."""
+    # the cosine similarity between two vectors is the dot product of the two vectors divided by the magnitude of each vector
+    
