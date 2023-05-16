840c77f4ecbc95ee20061291282ae2ac8d3b3cae
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-05-16 13:10:55 -0300

    feat: fill smaller vector with 0s

--- cosine-similarity.py
+++ cosine-similarity.py
@@ -3,4 +3,13 @@ def cosine_similarity(vector1: list[float], vector2: list[float]) -> float:
     # the cosine similarity between two vectors is the dot product of the two vectors divided by the magnitude of each vector
     
     dot_product = 0
-    largest_vector = vector1 if len(vector1) > len(vector2) else vector2
+
+    vector1_length = len(vector1)
+    vector2_length = len(vector2)
+
+    if vector1_length > vector2_length:
+        # fill vector2 with 0s until it is the same length as vector1 (required for dot product)
+        vector2.append(0) * (vector1_length - vector2_length)
+    elif vector2_length > vector1_length:
+        # fill vector1 with 0s until it is the same length as vector2 (required for dot product)
+        vector1.append(0) * (vector2_length - vector1_length)
