a170ad11612674277908104ec00875c862e59b18
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-05-16 13:18:07 -0300

    refactor(magnitude): replace ** for pow( ) function call

--- cosine-similarity.py
+++ cosine-similarity.py
@@ -1,3 +1,5 @@
+from math import sqrt, pow
+
 def cosine_similarity(vector1: list[float], vector2: list[float]) -> float:
     """Returns the cosine of the angle between two vectors."""
     # the cosine similarity between two vectors is the dot product of the two vectors divided by the magnitude of each vector
@@ -22,6 +24,6 @@ def cosine_similarity(vector1: list[float], vector2: list[float]) -> float:
 
     # vector1 magnitude calculation
     for i in range(len(vector1)):
-        magnitude_vector1 += vector1[i] ** 2
+        magnitude_vector1 += pow(vector1[i], 2)
         
 
