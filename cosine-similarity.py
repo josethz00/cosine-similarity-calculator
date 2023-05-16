74f94432a2e4302f7254f484954e08e225374fbc
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-05-16 13:24:52 -0300

    fix(fill vec): rewrite var value instead of appending

--- cosine-similarity.py
+++ cosine-similarity.py
@@ -11,12 +11,15 @@ def cosine_similarity(vector1: list[float], vector2: list[float]) -> float:
     vector1_length = len(vector1)
     vector2_length = len(vector2)
 
+    print(vector1_length)
+    print(vector2_length)
+
     if vector1_length > vector2_length:
         # fill vector2 with 0s until it is the same length as vector1 (required for dot product)
-        vector2.append(0) * (vector1_length - vector2_length)
+        vector2 = vector2 + [0] * (vector1_length - vector2_length)
     elif vector2_length > vector1_length:
         # fill vector1 with 0s until it is the same length as vector2 (required for dot product)
-        vector1.append(0) * (vector2_length - vector1_length)
+        vector1 = vector1 + [0] * (vector2_length - vector1_length)
 
     # dot product calculation
     for i in range(len(vector1)):
@@ -36,3 +39,5 @@ def cosine_similarity(vector1: list[float], vector2: list[float]) -> float:
     # return cosine similarity
     return dot_product / magnitude
 
+print(cosine_similarity([1, 2, 3], [4, 5, 6])) # 0.9746318461970762
+print(cosine_similarity([1, 0, 2], [0.7, 0, 1, 2])) # 0.7808688094430304s
